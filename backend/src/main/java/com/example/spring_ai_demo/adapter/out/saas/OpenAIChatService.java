5c052f563680baddbd76750bc57219da052304ac
HORIE Tetsuya <horie-t@users.noreply.github.com>
2025-12-06 20:13:45 +0900

    Spring Boot側で会話を記憶する

--- backend/src/main/java/com/example/spring_ai_demo/adapter/out/saas/OpenAIChatService.java
+++ backend/src/main/java/com/example/spring_ai_demo/adapter/out/saas/OpenAIChatService.java
@@ -1,6 +1,8 @@
 package com.example.spring_ai_demo.adapter.out.saas;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class OpenAIChatService {
     private final ApplicationContext context;
+    private final ChatMemory chatMemory;
     private final SyncMcpToolCallbackProvider syncMcpToolCallbackProvider;
 
-    public OpenAIChatService(ApplicationContext context, SyncMcpToolCallbackProvider syncMcpToolCallbackProvider) {
+    public OpenAIChatService(ApplicationContext context, ChatMemory chatMemory, SyncMcpToolCallbackProvider syncMcpToolCallbackProvider) {
         this.context = context;
+        this.chatMemory = chatMemory;
         this.syncMcpToolCallbackProvider = syncMcpToolCallbackProvider;
     }
 
@@ -26,6 +30,7 @@ public class OpenAIChatService {
     public String withPrompt(Prompt prompt) {
         ChatModel model = context.getBean(ChatModel.class);
         ChatClient client = ChatClient.builder(model)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .defaultToolCallbacks(syncMcpToolCallbackProvider).build();
         return client.prompt(prompt).call().content();
     }
