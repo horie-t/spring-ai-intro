9e0f795aec1eda0b7315f5bdf8f40b77e6367032
HORIE Tetsuya <horie-t@users.noreply.github.com>
2025-12-07 06:02:12 +0900

    バックエンドのリクエストで受け付ける型の名前をassistant-uiに合わせる

--- backend/src/main/java/com/example/spring_ai_demo/adapter/in/web/ChatController.java
+++ backend/src/main/java/com/example/spring_ai_demo/adapter/in/web/ChatController.java
@@ -1,7 +1,7 @@
 package com.example.spring_ai_demo.adapter.in.web;
 
 import com.example.spring_ai_demo.adapter.in.web.dto.AssistantUIContent;
-import com.example.spring_ai_demo.adapter.in.web.dto.AssistantUIMessage;
+import com.example.spring_ai_demo.adapter.in.web.dto.AssistantUIThreadMessage;
 import com.example.spring_ai_demo.adapter.out.saas.OpenAIChatService;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -20,7 +20,7 @@ public class ChatController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/api/chat")
-    public AssistantUIContent chat(@RequestBody AssistantUIMessage message) {
+    public AssistantUIContent chat(@RequestBody AssistantUIThreadMessage message) {
         return new AssistantUIContent(chatService.withPrompt(new Prompt(new UserMessage(message.getContent().getFirst().getText()))));
     }
 }
